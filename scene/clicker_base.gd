68e412bd6a746f1a6f7e21ce3481c6321cc305fe
seinosuke14 <diegobascur9@gmail.com>
2024-07-18 18:13:23 -0400

    Modificacion en la visibilidad de mejoras
    se crean 2 funciones nuevas.
    Estas son utilizadas tanto en presionar boton como en el timer, esto para que al momento de alcanzar los valores determinados se vuelvan visibles y funcionales

--- scene/clicker_base.gd
+++ scene/clicker_base.gd
@@ -7,16 +7,23 @@ var clickPerSecond = 0
 func _ready():
 	$LabelClick.text = str(click)
 	$LabelClickPerSecon.text = str(clickPerSecond) + " /s"
+	$"Mejora 1".visible = false
+	$"Mejora 2".visible = false
+	
+	
 
 func _on_texture_button_pressed():
 	click += 1
 	$LabelClick.text = str(click)
+	_MejoraVisility1()
+	_MejoraVisibility2()
 	
 
 
 func _on_timer_timeout():
 	click += clickPerSecond
 	$LabelClick.text = str(click)
+	_MejoraVisibility2()
 
 
 func _on_mejora_1_pressed():
@@ -30,6 +37,13 @@ func _on_mejora_2_pressed():
 	clickPerSecond += 5
 	$LabelClick.text = str(click)
 	$LabelClickPerSecon.text = str(clickPerSecond) + " /s"
-
+	
+	
+func _MejoraVisility1():
+	if click >= 10:
+		$"Mejora 1".visible = true
+func _MejoraVisibility2():
+	if click >= 20:
+		$"Mejora 2".visible = true
 
 
