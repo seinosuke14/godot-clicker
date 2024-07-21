ced433f110d8bccd369ae7ff453f5e0322f49c2d
seinosuke14 <diegobascur9@gmail.com>
2024-07-21 19:44:43 -0400

    se añaden niveles a las mejoras del clicker
    Se adiciona 2 labeltext de nivel para las mejoras disponibles

--- scene/clicker_base.gd
+++ scene/clicker_base.gd
@@ -9,7 +9,8 @@ var costMejora1 = 15
 var costMejora2 = 30
 var costClickMejora = 50
 var clickMejorado = 1
-
+var lvlMejora1 = 1
+var LvlMejora2 = 1
 
 func _ready():
 # Called when the node enters the scene tree for the first time.
@@ -20,11 +21,16 @@ func _ready():
 	$CostXclick.text = "Costo de Mejora: " + str(costClickMejora)
 	$"Mejora 1".visible = false
 	$"Mejora 2".visible = false
+	$Lvl_Cost_Mejora.visible = false
+	$Lvl_Cost_Mejora2.visible = false
+	
 	$Cost.visible = false
 	$Cost2.visible = false
 	$"Mejora 1".disabled = true
 	$"Mejora 2".disabled = true
 	$Mejora_X_Click.disabled = true
+	$Lvl_Cost_Mejora.text = "Lvl: " + str(lvlMejora1)
+	$Lvl_Cost_Mejora2.text= "Lvl: " + str(LvlMejora2)
 	#aqui se realiza la coneccion con la señal, mediante el "callable" o llamado al metodo y update_mejora_buttons
 	# es la func llamada
 	connect("click_updated",Callable(self, "_update_mejora_buttons"))
@@ -61,6 +67,7 @@ func _MejoraVisility1():
 	if click >= 10:
 		$"Mejora 1".visible = true
 		$Cost.visible = true
+		$Lvl_Cost_Mejora.visible = true
 	
 func _MejoraDisabled1():
 	if click >= costMejora1:
@@ -72,9 +79,11 @@ func _on_mejora_1_pressed():
 	clickPerSecond += 3
 	click = click - costMejora1
 	costMejora1 = int(costMejora1 * 1.6)
+	lvlMejora1 += 1
 	$LabelClick.text = str(click)
 	$LabelClickPerSecon.text = str(clickPerSecond) + " /s"
 	$Cost.text = str(costMejora1)
+	$Lvl_Cost_Mejora.text = "Lvl: " + str(lvlMejora1)
 	#aqui se decide usar la señal "click update" mencionada en el principio del codigo
 	emit_signal("click_updated")
 ##-------- FIN MEJORA 1 --------
@@ -85,6 +94,7 @@ func _MejoraVisibility2():
 	if click >= 20:
 		$"Mejora 2".visible = true
 		$Cost2.visible = true
+		$Lvl_Cost_Mejora2.visible = true
 
 func _MejoraDisabled2():
 	if click >= costMejora2:
@@ -95,6 +105,9 @@ func _on_mejora_2_pressed():
 	clickPerSecond += 5
 	click = click - costMejora2
 	costMejora2 = int(costMejora2 * 1.6)
+	LvlMejora2 += 1
+	
+	$Lvl_Cost_Mejora2.text = "Lvl: " + str(LvlMejora2)
 	$Cost2.text = str(costMejora2)
 	$LabelClick.text = str(click)
 	$LabelClickPerSecon.text = str(clickPerSecond) + " /s"
